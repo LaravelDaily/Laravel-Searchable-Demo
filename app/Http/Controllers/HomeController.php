a4911b5b02318001a474501b2cf3841b39d15c48
Povilas Korop <info@laraveldaily.com>
2018-12-28 11:47:17 +0200

    Whoops, no DD

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,8 +23,6 @@ class HomeController extends Controller
             ->registerModel(Category::class, 'name')
             ->perform($request->input('query'));
 
-        dd($searchResults, true);
-
         return view('search', compact('searchResults'));
     }
 
